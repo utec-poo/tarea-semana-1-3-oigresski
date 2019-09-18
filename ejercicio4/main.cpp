2005dacfaeadaf14109918d67db81f384117351a
oigresski <39835250+oigresski@users.noreply.github.com>
2019-09-18 10:25:56 -0500

    Update main.cpp

--- ejercicio4/main.cpp
+++ ejercicio4/main.cpp
@@ -2,17 +2,29 @@
 #include "Tipos.h"
 using namespace std;
 
-int main() {
-
-entero numero;
-caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+int main(){
+    char letra;
+    int numero;
+    int x;    
+    cout<<"letra:";
+    cin>>letra;
+    cout<<"numero:";
+    cin>>numero;
+    x=numero%2;
+   
+    if (letra== 'a' or 'c' or 'e' or 'g'){
+        
+        if (x==0)  
+            cout<<"es negro1";
+        else
+            cout<<"es blanco1";}
+    else{
+    if (letra== 'b' or 'd' or 'f' or 'h'){
+        if (x==0)
+            cout<<"es blanco2";
+        else
+           cout<<"es negro2";}}
+    
+    return 0;
+    
 }
