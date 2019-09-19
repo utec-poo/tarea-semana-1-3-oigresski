5ebbde4831bc4e6c6d6fcad5dab3e7e8a256fa2a
oigresski <39835250+oigresski@users.noreply.github.com>
2019-09-18 22:56:35 -0500

    Update main.cpp

--- ejercicio9/main.cpp
+++ ejercicio9/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include "tipos.h"
-#include "MyFunc.h"
 using namespace std;
-int main() {
-    l_enteros segundos;
-    enteros horas, min, seg;
+int cantseg,horas,min,seg,aux, mi;
+int main(){
+ cout << "introduce segundos: ";
+ cin >> cantseg;
+  //instrucciones matemáticas
+  horas = cantseg/3600;
+  aux = cantseg%3600;
+  mi = aux/60;
+  seg = aux%60;
+ cout << "horas " << horas;
+ cout <<  "minutos "<< mi;
+ cout << "segundos "<< seg;
 
-    do {
-        cout << "Segundos: ";
-        cin >> segundos;
-    } while (segundos <1);
-
-    // ToDO
-    return 0;
+ return 0;
 }
