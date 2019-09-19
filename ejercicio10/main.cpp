4e707bc9c05214ea1dbe8218226a48baffcb4a25
oigresski <39835250+oigresski@users.noreply.github.com>
2019-09-18 22:05:50 -0500

    Update main.cpp

--- ejercicio10/main.cpp
+++ ejercicio10/main.cpp
@@ -1,18 +1,26 @@
-#include <iostream>
-#include "Tipos.h"
+#include<iostream>
 using namespace std;
-
-int main() {
-
-entero numero;
-caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+int main(){
+ int num,aux,resto,numInv=0;
+ 
+ cout<<"Ingrese numero"<<endl;
+ cin>>num;
+ 
+ aux=num;
+ 
+ while(aux>0){
+         resto=aux%10;
+      aux=aux/10;
+      numInv=numInv*10+resto;
+ }
+ 
+ if(numInv==num){
+  cout<<"Numero es capicua";
+ }
+ else{
+  cout<<"Numero no capicua";
+  
+ }
+ 
+ return 0;
 }
