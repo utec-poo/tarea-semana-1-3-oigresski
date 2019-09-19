4110d7341acc6d0332e0547b8a7b0b901dd46d29
oigresski <39835250+oigresski@users.noreply.github.com>
2019-09-18 23:16:32 -0500

    Update main.cpp

--- ejercicio5/main.cpp
+++ ejercicio5/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
-int main() {
+int main(){
+  int contenedor;
+  int contadorA=0;
+  int contadorB=0;
 
-entero numero;
-caracter crt;
+  int num;
+  int suma=0;
 
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
+    do{ 
+   cout<<"ingrese varios numeros: "; 
+   cin>>num; 
+    
+   if(num%2==0){
+     contadorA++;
+     if(num==0)
+      contadorA--;
+     }
+   else
+    contadorB++;
+    }
+    while(num!=0);
+  
+ int total=contadorA+contadorB;
+ cout<<"numeros totales"<<total<<endl;
+ cout<<"numeros pares"<<contadorA<<endl; 
+ cout<<"numeros impares"<<contadorB<<endl;
+ return 0;
+    
 }
